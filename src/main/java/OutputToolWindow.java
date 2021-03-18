fc21637b07b1c050406c38a468c230dc3acd6760
Alessandro Fornaro <fornaroale@outlook.com>
2021-03-18 10:49:47 +0100

    Fixed GRID View and made grid's elements clickable

--- src/main/java/OutputToolWindow.java
+++ src/main/java/OutputToolWindow.java
@@ -1,16 +1,17 @@
 import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.ui.components.JBScrollPane;
 import models.CriticalMethod;
 import models.CriticalMethodTableModel;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.*;
 
 public class OutputToolWindow {
 
     private JButton hideToolWindowButton;
     private JPanel myToolWindowContent;
+    private JScrollPane outputScrollPane;
     private JTable outputTable;
 
     private CriticalMethodTableModel tableModel;
@@ -24,7 +25,6 @@ public class OutputToolWindow {
 
     private void readFiloCSV(){
         String pathToCsv = "C:\\Users\\ale\\IdeaProjects\\toolWindowExample\\src\\main\\resources\\FiloOutput.csv";
-        String outputText = "<html>";
 
         File csvFile = new File(pathToCsv);
         if (csvFile.isFile()) {
@@ -53,10 +53,27 @@ public class OutputToolWindow {
         outputTable.setModel(tableModel);
         outputTable.getColumnModel().getColumn(0).setMaxWidth(100);
 
-        // Table Scrollbar
-        JScrollPane scrollPane = new JBScrollPane(outputTable);
-        myToolWindowContent.setLayout(new BorderLayout());
-        myToolWindowContent.add(scrollPane, BorderLayout.CENTER);
+        outputTable.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                int row = outputTable.getSelectedRow();
+                int col = outputTable.getSelectedColumn();
+
+                String valueClicked = outputTable.getValueAt(row, col).toString();
+
+                if(!isNumeric(valueClicked))
+                    System.out.println("You clicked on method: " + valueClicked);
+            }
+        });
+
+    }
+
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
     }
 
     public JPanel getContent() {
